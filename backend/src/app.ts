7532de90196c3fc8fcf57dc854388d56ab242750
Pranab Kalita <pavankalita@gmail.com>
2023-11-27 21:29:40 +0530

    Installed and configured mongoose. Installed and configured envalid to make sure the env files have all and correct data

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+// Global Imports
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World !");
+});
+
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,19 @@
-import express from "express";
-const app = express();
-const port = 5000;
+// Global Imports
+import app from "./app";
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-  res.send("Hello, World !");
-});
+// Local Imports
+import env from "./utils/validateEnv";
 
-app.listen(port, () => {
-  console.log("Server running on port: " + port);
-});
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose Connected !");
+
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .then(console.error);
