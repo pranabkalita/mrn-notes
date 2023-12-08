453c2400d5cf8090b7f4883b60614b25529a9039
Pranab Kalita <pavankalita@gmail.com>
2023-12-08 09:41:05 +0530

    Added the delete note api

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -1,10 +1,18 @@
 import express from "express";
-import { getNotes, createNote, getNote } from "../controllers/notes";
+import {
+  getNotes,
+  createNote,
+  getNote,
+  updateNote,
+  deleteNote,
+} from "../controllers/notes";
 
 const router = express.Router();
 
 router.get("/", getNotes);
 router.get("/:noteId", getNote);
 router.post("/", createNote);
+router.patch("/:noteId", updateNote);
+router.delete("/:noteId", deleteNote);
 
 export default router;
